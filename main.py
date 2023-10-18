4b94c28f596d88c0c3673181e2fb2c0c84457d15
dev4ox <dev4ox@gmail.com>
2023-10-18 20:12:16 +0300

    upload changes 18.10.2023

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ import key
 import menu
 import text
 
-bot = telebot.TeleBot(key.tgtoken)
+bot = telebot.TeleBot(key.bot_token)
 def bot_start():
     @bot.message_handler(commands=['start'])
     def command_start(message):
-        user = [message.from_user.id, message.from_user.first_name, message.message_id]
-        bot.send_message(user[0], f'<b>Здравствуйте, {user[1]}</b>\n\n'+text.start, 'html', reply_markup=menu.start())
+        user = [message.chat.id, message.from_user.first_name, message.message_id]
+        bot.send_message(user[0], text.main.format(user[1]), parse_mode='html', reply_markup=menu.main())
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_process(call):
-        user = [call.message.from_user.id, call.message.from_user.first_name, call.message.message_id]
-        if call.data == 'order':
-            bot.edit_message_text(user[0], 'Мы позвоним вам.\nДля этого разрешите боту просмотр вашего номера телефона или напишите свой номер:')
+        user = [call.message.chat.id, call.message.from_user.first_name, call.message.message_id]
+        if call.data == 'main':
+            bot.edit_message_text(text.main.format(user[1]), user[0], user[2], parse_mode='html',
+                                  reply_markup=menu.main())
+        elif call.data == 'order':
+            bot.edit_message_text(text.order, user[0], user[2], parse_mode='html', reply_markup=menu.order())
         elif call.data == 'lk':
-            bot.send_message(user[0], )
-
+            bot.edit_message_text(text.lk.format(user[1], user[0]), user[0], user[2], parse_mode='html',
+                                  reply_markup=menu.lk())
+        elif call.data == 'catalog':
+            pass
+        elif call.data == 'user_data':
+            pass
+        elif call.data == 'user_history':
+            pass
+        else:
+            bot.edit_message_text(text.main.format(user[1]), user[0], user[2], parse_mode='html',
+                                  reply_markup=menu.main())
 
     bot.polling(none_stop=True)
 
--- menu.py
+++ menu.py
@@ -1,8 +1,26 @@
 from telebot import types
-def start():
+def main():
     markup = types.InlineKeyboardMarkup(row_width=2)
     button_1 = types.InlineKeyboardButton('📝  Сделать заказ', callback_data='order')
-    button_2 = types.InlineKeyboardButton('📞  Консультация', url='https://t.me/dev4ox')
+    button_2 = types.InlineKeyboardButton('Канал в telegram  📢', url='https://t.me/rndfoxservice')
     button_3 = types.InlineKeyboardButton('🔑  Личный кабинет', callback_data='lk')
     markup.add(button_1, button_2, button_3)
     return markup
+
+def lk():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    button_1 = types.InlineKeyboardButton('⬅️  Назад', callback_data='main')
+    button_2 = types.InlineKeyboardButton('👤  Мои данные', callback_data='user_data')
+    button_3 = types.InlineKeyboardButton('  История заказов', callback_data='user_history')
+    markup.add(button_1, button_2, button_3)
+    return markup
+
+def order():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    button_1 = types.InlineKeyboardButton('⬅️  Назад', callback_data='main')
+    button_2 = types.InlineKeyboardButton('📋  Каталог', callback_data='catalog')
+    button_3 = types.InlineKeyboardButton('📩  Написать нам', url='https://t.me/dev4ox')
+    markup.add(button_1, button_2, button_3)
+    return markup
+
+
